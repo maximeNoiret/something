07ab7f1b270488b003a411639fbeb8d64d077ab4
NOIRET Maxime <maxime.noiret@etu.univ-amu.fr>
2026-03-05 05:42:49 +0100

    mix: See description
    chore: Added functions, was just too ugly not to
    dev: Added type to variables
    dev: Implemented print
    dev: Added comments to resulting assembly code

--- c-compiler/main.c
+++ c-compiler/main.c
@@ -7,10 +7,33 @@
 #define SYMB_LIM 64
 
 typedef struct Symbol {
+  char type[16];
   char name[32];
   unsigned offset;
 } Symbol;
 
+Symbol symbols[SYMB_LIM];
+
+unsigned getVariableOffset(char *name) {
+  unsigned offset = -1;
+  for (unsigned i = 0; i < SYMB_LIM && symbols[i].name[0] != '\0'; ++i) {
+    if (strcmp(symbols[i].name, name) == 0) {
+      offset = symbols[i].offset;
+      break;
+    }
+  }
+  return offset;
+}
+
+void getVariableType(char *name, char *buf) {
+  memcpy(buf, "null", 5);
+  for (unsigned i = 0; i < SYMB_LIM && symbols[i].name[0] != '\0'; ++i) {
+    if (strcmp(symbols[i].name, name) == 0) {
+      strncpy(buf, symbols[i].type, 16);
+      return;
+    }
+  }
+}
 
 // NOTE: the compiler will not have functions until I actually implement that in the compiler. This is so that bootstrapping is easier.
 // Apologies in advance for the (probably) messy code that will ensue from this choice.
@@ -76,7 +99,6 @@ int main(int argc, char **argv) {
 
   unsigned current_sp = symbol_size;
   unsigned symbol_counts = 0;
-  Symbol symbols[SYMB_LIM];
 
   unsigned line_count = 1;
   // SECOND PASS for the REAL code. (this is so bad help)
@@ -96,48 +118,60 @@ int main(int argc, char **argv) {
       else if (wrd == 1 && strcasecmp(buffer[0], "goto") == 0) {  // goto
         fprintf(output, "j %s\n", buffer[1]);
       }
-      else if (wrd == 4 && strcasecmp(buffer[0], "if") == 0) {    // if statement
-        // get offset
-        unsigned offset = -1;
-        for (unsigned i = 0; i < SYMB_LIM && symbols[i].name[0] != '\0'; ++i) {
-          if (strcmp(symbols[i].name, buffer[1]) == 0) {
-            offset = symbols[i].offset;
-          }
-        }
+      else if (wrd == 1 && strcasecmp(buffer[0], "print") == 0) { // print
+        unsigned offset = getVariableOffset(buffer[1]);
         if (offset == -1) {
           fprintf(stderr, "Something went wrong: Unknown variable.\n\tLine: %d\n\t'%s'\n", line_count, buffer[1]);
           return -1;
         }
-        fprintf(output, "\nlw  $t0, %d($sp)\nbltz $t0, %s\nbgtz $t0, %s\nbeq $t0, $zero, %s\n", offset, buffer[2], buffer[4], buffer[3]);
+        char buf[16];
+        getVariableType(buffer[1], buf);
+        if (strcmp(buf, "null") == 0) {
+          fprintf(stderr, "Something went wrong: Type is null.\n\tLine: %d\n\t'%s'\n", line_count, buffer[1]);
+          return -1;
+        }
+
+        // TODO: do more types
+        if (strcasecmp(buf, "int") == 0) {
+          fprintf(output, "# print %s\naddi $sp, $sp, -8\nsw  $v0, 4($sp)\nsw  $a0, 0($sp)\nori $v0, $zero, 1\nlw  $a0, %d($sp)\nsyscall\n# newline\nori $v0, $zero, 11\nori  $a0, $zero, 10\nsyscall\nlw  $a0, 0($sp)\nlw  $v0, 4($sp)\naddi $sp, $sp, 8\n", buffer[1], offset+8);
+        }
+      }
+      else if (wrd == 4 && strcasecmp(buffer[0], "if") == 0) {    // if statement
+        unsigned offset = getVariableOffset(buffer[1]);
+        if (offset == -1) {
+          fprintf(stderr, "Something went wrong: Unknown variable.\n\tLine: %d\n\t'%s'\n", line_count, buffer[1]);
+          return -1;
+        }
+        fprintf(output, "# if %s\nlw  $t0, %d($sp)\nbltz $t0, %s\nbgtz $t0, %s\nbeq  $t0, $zero, %s\n", buffer[1], offset, buffer[2], buffer[4], buffer[3]);
       }
       else if (wrd == 3 && buffer[2][0] == '=') {                 // variable creation
-        // get offset
-        unsigned offset = -1;
-        for (unsigned i = 0; i < SYMB_LIM && symbols[i].name[0] != '\0'; ++i) {
-          if (strcmp(symbols[i].name, buffer[1]) == 0) {
-            offset = symbols[i].offset;
-          }
-        }
+        unsigned offset = getVariableOffset(buffer[1]);
         if (offset != -1) {
           fprintf(stderr, "Something went wrong: Variable exists.\n\tLine: %d\n\t'%s'\n", line_count, buffer[1]);
           return -1;
         }
-        // TODO: differentiate variable types and sizes. FOR NOW everything is int (4 bytes).
 
-        current_sp -= 4;
+        // TODO: check that TYPE is a correct type
+        current_sp -= 4;  // TODO: different size per type.
         // TODO: get rid of that icky strcpy ;-;
         strcpy(symbols[symbol_counts].name, buffer[1]);
+        strcpy(symbols[symbol_counts].type, buffer[0]);
         symbols[symbol_counts++].offset = current_sp;
         fprintf(output, "ori $t0, $zero, %s\nsw  $t0, %d($sp)\n", buffer[3], current_sp);
       }
-      else if (wrd == 2 && buffer[1][0] == '=') {                 // variable set
-        // get offset
-        unsigned offset = -1;
-        for (unsigned i = 0; i < SYMB_LIM && symbols[i].name[0] != '\0'; ++i) {
-          if (strcmp(symbols[i].name, buffer[0]) == 0) {
-            offset = symbols[i].offset;
-          }
+      else if (wrd == 2 && (buffer[1][0] == '+' || buffer[1][0] == '-')) {
+        unsigned offset = getVariableOffset(buffer[0]);
+        if (offset == -1) {
+          fprintf(stderr, "Something went wrong: Unknown variable.\n\tLine: %d\n\t'%s'\n", line_count, buffer[1]);
+          return -1;
         }
+        fprintf(output, "lw  $t0, %d($sp)\naddi $t0, $t0, ", offset);
+        if (buffer[1][0] == '-')
+          fputc('-', output);
+        fprintf(output, "%s\nsw  $t0, %d($sp)\n", buffer[2], offset);
+      }
+      else if (wrd == 2 && buffer[1][0] == '=') {                 // variable set
+        unsigned offset = getVariableOffset(buffer[0]);
         if (offset == -1) {
           fprintf(stderr, "Something went wrong: Unknown variable.\n\tLine: %d\n\t'%s'\n", line_count, buffer[0]);
           return -1;
